4c20c70366d9fe9e7d30ee9c5cd6f17873710f65
william <alkamimw@gmail.com>
2026-03-23 10:29:38 -0300

    aulas 204 ate a 205

---
+++ 18 - Interfaces/aula204/entities/Circle.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+public class Circle extends AbstractShape {
+    private Double radius;
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double area() {
+        return  Math.PI * radius * radius;
+    }
+}
