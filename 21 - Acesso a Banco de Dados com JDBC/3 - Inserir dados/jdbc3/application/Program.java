f77e30bc027f49aefb3c3b0a09faf84edf291e9b
willthepc <alkamimw@gmail.com>
2026-06-04 20:06:51 -0300

    novos cap de jdbc

--- 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/application/Program.java
+++ 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/application/Program.java
@@ -14,26 +14,37 @@ public class Program {
 
         try {
             conn = DB.getConnection();
+//
+//            st = conn.prepareStatement(
+//                    "INSERT INTO seller "
+//                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+//                            + "VALUES "
+//                            + "(?, ?, ?, ?, ?)",
+//                            Statement.RETURN_GENERATED_KEYS);
+//
+//            st.setString(1, "Carl Purple");
+//            st.setString(2, "carl@gmail.com");
+//            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+//            st.setDouble(4, 2300.0);
+//            st.setInt(5, 4);
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                            + "VALUES "
-                            + "(?, ?, ?, ?, ?)");
-
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 2300.0);
-            st.setInt(5, 4);
+                    "insert into department (Name) values ('D1'), ('D2')",
+                    Statement.RETURN_GENERATED_KEYS);
 
             int rowsAffected = st.executeUpdate();
 
-            System.out.println("Done! Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else {
+                System.out.println("No rown affected!");
+            }
 
         } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
             e.printStackTrace();
         } finally {
             DB.closeStatement(st);
---
+++ 21 - Acesso a Banco de Dados com JDBC/4 - Atualizar dados/jdbc4/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import db.DB;
+
+import java.sql.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET BaseSalary = BaseSalary + ? "
+                            + "WHERE "
+                            + "(DepartmentId = ?)");
+
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
