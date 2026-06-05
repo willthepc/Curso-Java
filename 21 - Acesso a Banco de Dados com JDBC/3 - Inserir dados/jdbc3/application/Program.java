025f63e87e7186035cd5586279fa319834e0c6df
william <alkamimw@gmail.com>
2026-06-04 22:34:23 -0300

    mudança de path

--- 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/application/Program.java
+++ 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/application/Program.java
@@ -2,7 +2,11 @@ package application;
 
 import db.DB;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -14,24 +18,24 @@ public class Program {
 
         try {
             conn = DB.getConnection();
-//
-//            st = conn.prepareStatement(
-//                    "INSERT INTO seller "
-//                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-//                            + "VALUES "
-//                            + "(?, ?, ?, ?, ?)",
-//                            Statement.RETURN_GENERATED_KEYS);
-//
-//            st.setString(1, "Carl Purple");
-//            st.setString(2, "carl@gmail.com");
-//            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-//            st.setDouble(4, 2300.0);
-//            st.setInt(5, 4);
 
             st = conn.prepareStatement(
-                    "insert into department (Name) values ('D1'), ('D2')",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
 
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 2300.0);
+            st.setInt(5, 4);
+
+//            st = conn.prepareStatement(
+//                    "insert into department (Name) values ('D1'), ('D2')",
+//                    Statement.RETURN_GENERATED_KEYS);
+//
             int rowsAffected = st.executeUpdate();
 
             if (rowsAffected > 0) {
@@ -46,7 +50,11 @@ public class Program {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
--- 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/db/DB.java
+++ 21 - Acesso a Banco de Dados com JDBC/3 - Inserir dados/jdbc3/db/DB.java
@@ -2,7 +2,11 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
